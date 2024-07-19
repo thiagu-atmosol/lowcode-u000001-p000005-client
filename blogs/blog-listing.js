6430cf991fde9a2b3c07c82fe71e1a93767eddad
Pratik Chaudhari <pratik.chaudhari@inncrewin.com>
2024-07-19 12:18:53 +0530

    Code updated for the deployment id:

--- blogs/blog-listing.js
+++ blogs/blog-listing.js
@@ -3,32 +3,32 @@ console.log("Hello from blog-listing.js!");
 document.addEventListener("DOMContentLoaded", (event) => {
 
     
-        let blogInfoDiv = document.querySelector('#blogInfo-'6');
+        blogInfoDiv = document.querySelector('#blogInfo-6');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 6 Div was clicked!');
         });
     
-        let blogInfoDiv = document.querySelector('#blogInfo-'9');
+        blogInfoDiv = document.querySelector('#blogInfo-9');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 9 Div was clicked!');
         });
     
-        let blogInfoDiv = document.querySelector('#blogInfo-'15');
+        blogInfoDiv = document.querySelector('#blogInfo-15');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 15 Div was clicked!');
         });
     
-        let blogInfoDiv = document.querySelector('#blogInfo-'30');
+        blogInfoDiv = document.querySelector('#blogInfo-30');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 30 Div was clicked!');
         });
     
-        let blogInfoDiv = document.querySelector('#blogInfo-'36');
+        blogInfoDiv = document.querySelector('#blogInfo-36');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 36 Div was clicked!');
         });
     
-        let blogInfoDiv = document.querySelector('#blogInfo-'38');
+        blogInfoDiv = document.querySelector('#blogInfo-38');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 38 Div was clicked!');
         });
