cbddbaf714e8d395db0c8b46ae6df32544f1d0cc
Pratik Chaudhari <pratik.chaudhari@inncrewin.com>
2024-07-19 12:45:34 +0530

    Code updated for the deployment id:

--- blogs/blog-listing.js
+++ blogs/blog-listing.js
@@ -6,31 +6,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-6');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 6 Div was clicked!');
+            if(Start textile business != null || Start textile business != ''){
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/${Start textile business.replace(" ", "-").toLowerCase()}.html`;
+            }
+            else{
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/p${1}b${6}.html`;
+            }
+
         });
     
         blogInfoDiv = document.querySelector('#blogInfo-9');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 9 Div was clicked!');
+            if(Gym and fitness blog part 1 != null || Gym and fitness blog part 1 != ''){
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/${Gym and fitness blog part 1.replace(" ", "-").toLowerCase()}.html`;
+            }
+            else{
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/p${1}b${9}.html`;
+            }
+
         });
     
         blogInfoDiv = document.querySelector('#blogInfo-15');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 15 Div was clicked!');
+            if(black friday deals != null || black friday deals != ''){
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/${black friday deals.replace(" ", "-").toLowerCase()}.html`;
+            }
+            else{
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/p${1}b${15}.html`;
+            }
+
         });
     
         blogInfoDiv = document.querySelector('#blogInfo-30');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 30 Div was clicked!');
+            if(Crypto insights and Understandings != null || Crypto insights and Understandings != ''){
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/${Crypto insights and Understandings.replace(" ", "-").toLowerCase()}.html`;
+            }
+            else{
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/p${1}b${30}.html`;
+            }
+
         });
     
         blogInfoDiv = document.querySelector('#blogInfo-36');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 36 Div was clicked!');
+            if( != null ||  != ''){
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/${.replace(" ", "-").toLowerCase()}.html`;
+            }
+            else{
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/p${1}b${36}.html`;
+            }
+
         });
     
         blogInfoDiv = document.querySelector('#blogInfo-38');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 38 Div was clicked!');
+            if( != null ||  != ''){
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/${.replace(" ", "-").toLowerCase()}.html`;
+            }
+            else{
+                window.location.href = `${https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs}/p${1}b${38}.html`;
+            }
+
         });
     
 });
