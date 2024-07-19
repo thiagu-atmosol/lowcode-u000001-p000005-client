a6cf1f89944647cc0b742934dc8cfcd6acf0d885
Pratik Chaudhari <pratik.chaudhari@inncrewin.com>
2024-07-19 12:58:40 +0530

    Code updated for the deployment id:

--- blogs/blog-listing.js
+++ blogs/blog-listing.js
@@ -6,8 +6,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-6');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 6 Div was clicked!');
-            if(true  || true ){
-                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/Start textile business.replace(" ", "-").toLowerCase().html`;
+            if(true){
+                blogName = Start textile business;
+                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/${blogName.replace(" ", "-").toLowerCase()}.html`;
             }
             else{
                 window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/p1b6.html`;
@@ -18,8 +19,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-9');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 9 Div was clicked!');
-            if(true  || true ){
-                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/Gym and fitness blog part 1.replace(" ", "-").toLowerCase().html`;
+            if(true){
+                blogName = Gym and fitness blog part 1;
+                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/${blogName.replace(" ", "-").toLowerCase()}.html`;
             }
             else{
                 window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/p1b9.html`;
@@ -30,8 +32,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-15');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 15 Div was clicked!');
-            if(true  || true ){
-                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/black friday deals.replace(" ", "-").toLowerCase().html`;
+            if(true){
+                blogName = black friday deals;
+                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/${blogName.replace(" ", "-").toLowerCase()}.html`;
             }
             else{
                 window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/p1b15.html`;
@@ -42,8 +45,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-30');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 30 Div was clicked!');
-            if(true  || true ){
-                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/Crypto insights and Understandings.replace(" ", "-").toLowerCase().html`;
+            if(true){
+                blogName = Crypto insights and Understandings;
+                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/${blogName.replace(" ", "-").toLowerCase()}.html`;
             }
             else{
                 window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/p1b30.html`;
@@ -54,8 +58,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-36');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 36 Div was clicked!');
-            if(false  || true ){
-                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/.replace(" ", "-").toLowerCase().html`;
+            if(true){
+                blogName = ;
+                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/${blogName.replace(" ", "-").toLowerCase()}.html`;
             }
             else{
                 window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/p1b36.html`;
@@ -66,8 +71,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         blogInfoDiv = document.querySelector('#blogInfo-38');
         blogInfoDiv.addEventListener('click', function() {
             console.log('Blog 38 Div was clicked!');
-            if(false  || true ){
-                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/.replace(" ", "-").toLowerCase().html`;
+            if(true){
+                blogName = ;
+                window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/${blogName.replace(" ", "-").toLowerCase()}.html`;
             }
             else{
                 window.location.href = `https://lowcodeappsourcestore.z7.web.core.windows.net/u1p5/blogs/p1b38.html`;
